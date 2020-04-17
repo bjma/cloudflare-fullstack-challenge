90f9e3e7d5007ec4cc4240cf183d8302ce9a3251
Brian Ma <jingjerma@gmail.com>
2020-04-16 19:25:30 -0700

    added HTMLRewrite methods

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 // Event listener for catching FetchEvents
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
-})
+});
+
+/**
+ * Handler for incoming elements
+ */
+class ElementHandler {
+    constructor(attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    // Changes attribute value if @param attributeName is an attribute,
+    // else, do nothing.
+    element(element) {
+        // Get attribute as String
+        const attribute = element.getAttribute(this.attributeName);
+        // If attribute is actually found
+        if (attribute) {
+            // Set 'href' attribute to value: 'https://bjma.github.io'
+            // It's not a complete website, I know :(
+            element.setAttribute(
+                this.attributeName,
+                attribute.replace('https://cloudflare.com', 'https://www.linkedin.com/in/brian-j-ma')
+            );
+        }
+    }
+
+    // Change text value of current `element` to new text
+    text(text) {
+        if (!text.lastInTextNode) {
+            if (text.text.includes("Return to cloudflare.com")) { // change <a> text
+                text.replace('LinkedIn');
+            }
+
+            if (text.text.includes("Variant")) { // change <h1> text
+                text.replace('Hi, Cloudflare recruitment team!');
+            }
+
+            if (text.text.includes("take home project")) { // replace <p> text
+                text.replace('Thanks for this fun challenge!')
+            }
+        } else {
+            text.replace('');
+        }
+        
+    }
+
+
+}
 
 /**
  * Returns one of two URL variants as new Response 
@@ -16,25 +63,30 @@ async function handleRequest(request) {
         const response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
         // Read our response as text
         const text = await response.text();
-        //console.log("this is to confirm u got it lol");
-        //console.log(text);
         // Parse text to JSON
         const urls = JSON.parse(text);
         
+        // Init new HTMLRewriter that handles
+        // <a> tags; we use this to change 'href'
+        // and 'text' values
+        const rewriter = new HTMLRewriter()
+            .on('a#url', new ElementHandler('href'))
+            .on('h1#title', new ElementHandler())
+            .on('p#description', new ElementHandler());
         // Randomly redirect user to either variant A/B with a 50/50 chance
-        //return (Math.random() < 0.5) ? new Response(urls.variant[0].url) : new Response(urls.variant[1]);
         if (Math.random() < 0.5) {
-            return await fetch(urls.variants[0]); // A
-
+            // Variant 1
+            const A = await fetch(urls.variants[0]); 
+            //return A;
+            return rewriter.transform(A);
         } else {
-            return await fetch(urls.variants[1]); // B
+            // Variant 2
+            const B = await fetch(urls.variants[1]);
+            //return B;
+            return rewriter.transform(B);
         }
         
     } catch(err) {
         console.log(err);
     }
-
-    /*return new Response('Hello worker!', {
-        headers: { 'content-type': 'text/plain' },
-    });*/
 }
